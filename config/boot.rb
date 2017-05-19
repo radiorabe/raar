494785f4f45132c2f67e89561554b2e077a9991b
Pascal Zumkehr <pascal@codez.ch>
2017-05-19 21:21:07 +0200

    override rails server default options to always listen on all addresses

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,14 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+require 'rails/command'
+require 'rails/commands/server/server_command'
+
+module Rails
+  class Server
+    def default_options
+      super.merge(Host: '0.0.0.0', Port: 3000)
+    end
+  end
+end
